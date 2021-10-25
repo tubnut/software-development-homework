c590087e22c0cc021bb1b03874edbecd93a5e23a
tubnut <catwithahandgun@gmail.com>
2021-10-25 15:55:18 -0500

    starting to fix logic on removing -99 as the smallest and fixing the largest

--- Chapter 5/chapter_5_hw_1.cpp
+++ Chapter 5/chapter_5_hw_1.cpp
@@ -3,11 +3,25 @@ using namespace std;
 
 int main(){
     int input = 0;
+    int largest, smallest;
 
+    cout << "Enter a number: ";
+    
     while(input != -99){
         cout << "Enter a number: ";
         cin >> input;
+
+        if (input < smallest){
+            smallest = input;
+        }
+
+        if (input > largest){
+            largest = input;
+        }
     }
     
+    cout << "The Largest is: " << largest << endl;
+    cout << "The Smallest is " << smallest << endl;
+
     return 0;
 }
